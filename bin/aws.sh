1e6dbfbcc8c5c855e3da90c255a43c2adf0b55fb
Jack Lau <jackpocket@gmail.com>
2015-12-25 18:13:35 +0800

    add aws lambda alias

--- bin/aws.sh
+++ bin/aws.sh
@@ -2,6 +2,12 @@ alias s3ls='aws s3 ls --human-readable --summarize'
 alias s3cp='aws s3 cp'
 alias s3mv='aws s3 mv'
 alias s3rm='aws s3 rm'
+alias lambdalsv='aws lambda list-versions-by-function --function-name'
+alias lambdalsa='aws lambda list-aliases --function-name'
+
+function lambdasa {
+    aws lambda update-alias --function-name $1 --name $2 --function-version $3
+}
 
 alias dyodblist='aws dynamodb list-tables'
 alias dyodbdesc='aws dynamodb describe-table --table-name'
